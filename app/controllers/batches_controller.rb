a24b57f9433d851402238cc2bb10333f7946191a
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-02-20 18:48:39 +0100

    Implement BatchesController’s create method

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -2,4 +2,15 @@ class BatchesController < ApplicationController
   def new
     @batch = Batch.new
   end
+
+  def create
+    batch = Batch.new
+
+    batch.description = params[:batch][:spreadsheet].original_filename
+    batch.assembly = params[:batch][:assembly]
+
+    batch.save
+
+    redirect_to batch
+  end
 end
