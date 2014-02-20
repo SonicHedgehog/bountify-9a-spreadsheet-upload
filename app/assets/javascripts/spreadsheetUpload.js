5a8dd7ba65ab4faa1f603ace7b59270bf29c27a1
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-02-20 20:30:27 +0100

    Make Angular work with Rails’ CSRF protection

--- app/assets/javascripts/spreadsheetUpload.js
+++ app/assets/javascripts/spreadsheetUpload.js
@@ -1,4 +1,7 @@
 angular.module('spreadsheetUpload', ['angularFileUpload'])
+  .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
+  }])
   .controller('UploadCtrl', ['$scope', '$upload', function($scope, $upload) {
     $scope.onFileSelect = function($files) {
       for(var i = 0; i < $files.length; i++) {
