b2c0c2297ecbd2bee3b37b5d901359dd3a8b57d8
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-02-20 18:02:54 +0100

    Make batches a resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SpreadsheetUpload::Application.routes.draw do
+  resources :batches
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
