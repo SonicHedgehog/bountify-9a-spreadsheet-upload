888ae906999c760a872932a0596cc3d6d84876fd
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-02-20 19:48:25 +0100

    Use Roo to parse spreadsheet and create BatchDetail objects

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,3 +1,5 @@
+require 'roo'
+
 class BatchesController < ApplicationController
   def new
     @batch = Batch.new
@@ -8,6 +10,19 @@ class BatchesController < ApplicationController
 
     batch.description = params[:batch][:spreadsheet].original_filename
     batch.assembly = params[:batch][:assembly]
+
+    spreadsheet = Roo::CSV.new(params[:batch][:spreadsheet].path)
+    spreadsheet_header = spreadsheet.row(1)
+
+    (2..spreadsheet.last_row).each do |i|
+      batch_detail = batch.batch_details.new
+      # Alternative: row = Hash[*spreadsheet_header.zip(spreadsheet.row(i)).flatten]
+      row = Hash[[spreadsheet_header, spreadsheet.row(i)].transpose]
+
+      batch_detail.chrom = row['chrom']
+      batch_detail.chrom_start = row['chrom_start'].to_i
+      batch_detail.chrom_end = row['chrom_end'].to_i
+    end
 
     batch.save
 
